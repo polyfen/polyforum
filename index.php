ffc201ebfc2c19b0224dbbe21bfed15340e98b9d
Boris Hrncic <services@borishrncic.com>
2019-04-10 13:03:27 -0300

    WIP

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
       <div class="row" style="padding: 150px 0 0px;">
         <div class="col">
 
-          <h1>An international<br/>community of designers</h1>
+          <h1>An international<br/>community of creatives</h1>
           <hr style="width:300px;margin: 1.2rem auto;"/>
           <p>A place to discuss branding, share tips and resources,<br/>ask questions, and request feedback.</p>
           <a class="button" href="https://join.slack.com/t/polyforum/shared_invite/enQtNjA3Nzc3ODU0NzU4LTBiYzNiY2JjZmJmMWE4ZjNjZGZhMzI0M2QzNGI3OGYxMjYzMzVjMTY2ODZmY2U3NDZiNzUxZWY0YTRjODY2YzI">Join us in Slack</a>
