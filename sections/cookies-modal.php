ec074159b3d48ae605b3083facc5f5ad0bb0ad00
Boris Hrnčić <borishrncic@users.noreply.github.com>
2022-10-22 02:46:11 +0200

    Hotfix

--- sections/cookies-modal.php
+++ sections/cookies-modal.php
@@ -1,3 +1,3 @@
 <div id="cookies-modal">
-    Improve your experience accepting our cookies policy. <div id="cookies-modal-buttons"><button>👍 Accept cookies</button> <button onclick="document.getElementById('cookies-modal').className='cookies-d-none';">👎 Reject cookies</button></div>
+    Improve your experience accepting our cookies policy. <div id="cookies-modal-buttons"><button onclick="document.getElementById('cookies-modal').className='cookies-d-none';">👍 Accept cookies</button> <button onclick="document.getElementById('cookies-modal').className='cookies-d-none';">👎 Reject cookies</button></div>
 </div>
