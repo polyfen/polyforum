8c89b24366591a66e2e9b84093837e602f97d293
Boris Hrnčić <borishrncic@users.noreply.github.com>
2022-11-09 02:22:24 +0100

    Replaced Intro Section with subscription form

--- index.php
+++ index.php
@@ -72,14 +72,46 @@
         <div class="col">
           <div class="row card-wrapper">
             <div class="col-12" style="padding-bottom:10px;">
-              <h2>Empowering passion</h2>
+              <h2>Empower passion!</h2>
               <div class="border" style="margin-top:-10px;">
               </div>
               <div class="col-12 col-md-6" id="education-image">
               </div>
               <div id="polyatlas-wrapper" class="col-12 col-md-6">
-                <p style="margin-top:30px;">Our mission is to use new technologies to make education and professional growth more accessible in the creative industry world-wide.</p>
-              </div>
+              <p>
+                  Support our community and become a core member by staying up to date with our email newsletter.
+                </p>
+
+                <div id="form-container">
+                  <!-- Begin Mailchimp Signup Form -->
+                <div id="mc_embed_signup">
+                  <form action="https://polyforum.us13.list-manage.com/subscribe/post?u=1c1543196ef746efeaee1ba01&amp;id=0be410489f&amp;f_id=00e0dee2f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_self">
+                          <div id="mc_embed_signup_scroll">
+                  <div class="mc-field-group">
+                    <input type="text" placeholder="First name" value="" name="FNAME" class="required" id="mce-FNAME" required>
+                    <span id="mce-FNAME-HELPERTEXT" class="helper_text"></span>
+                  </div>
+                  <div class="mc-field-group">
+                    <input type="email" placeholder="Email address" value="" name="EMAIL" class="required email" id="mce-EMAIL" required>
+                    <span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span>
+                  </div>
+                  
+                  <div hidden="true"><input type="hidden" name="tags" value="6665147"></div>
+                    <div id="mce-responses" class="clear foot">
+                      <div class="response" id="mce-error-response" style="display:none"></div>
+                      <div class="response" id="mce-success-response" style="display:none"></div>
+                    </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                      <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_1c1543196ef746efeaee1ba01_0be410489f" tabindex="-1" value=""></div>
+                          <div class="optionalParent">
+                              <div class="clear foot">
+                                  <input type="submit" value="Get our News" name="subscribe" id="mc-embedded-subscribe" class="button">
+                                  <p class="brandingLogo" style="display:none;"><a href="http://eepurl.com/idfsm9" title="Mailchimp - email marketing made easy and fun"><img src="https://eep.io/mc-cdn-images/template_images/branding_logo_text_dark_dtp.svg"></a></p>
+                              </div>
+                          </div>
+                      </div>
+                  </form>
+                  </div>
+                  <!--End mc_embed_signup-->
             </div>
           </div>
         </div>
---
+++ newsletter.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- INCLUDE HEAD.PHP start -->
+  <?php include 'sections/head.php';?>
+<!-- INCLUDE HEAD.PHP end -->
+
+  <body id="home">
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P8K76NG"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+    <!-- INCLUDE HEADER.PHP start -->
+      <?php include 'sections/header.php';?>
+    <!-- INCLUDE HEADER.PHP end -->
+
+    <div class="container-fluid" style="padding: 0 20px;">
+
+      <div class="row">
+        <div class="col">
+          <div class="row card-wrapper">
+            <div class="col-12" style="padding-bottom:10px;">
+            <h2>Empower passion!</h2>
+              <div class="border" style="margin-top:-10px;">
+              </div>
+
+              <div class="col-12 col-md-6" id="education-image">
+                
+              </div>
+
+              <div id="polyatlas-wrapper" class="col-12 col-md-6">
+
+                <p>
+                  Support our community and become a core member by staying up to date with our email newsletter.
+                </p>
+
+                <div id="form-container">
+                  <!-- Begin Mailchimp Signup Form -->
+                <div id="mc_embed_signup">
+                  <form action="https://polyforum.us13.list-manage.com/subscribe/post?u=1c1543196ef746efeaee1ba01&amp;id=0be410489f&amp;f_id=00e0dee2f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_self">
+                          <div id="mc_embed_signup_scroll">
+                  <div class="mc-field-group">
+                    <label for="mce-FNAME">First name
+                    </label>
+                    <input type="text" value="" name="FNAME" class="required" id="mce-FNAME" required>
+                    <span id="mce-FNAME-HELPERTEXT" class="helper_text"></span>
+                  </div>
+                  <div class="mc-field-group">
+                    <label for="mce-EMAIL">Email address
+                    </label>
+                    <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" required>
+                    <span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span>
+                  </div>
+                  
+                  <div hidden="true"><input type="hidden" name="tags" value="6665147"></div>
+                    <div id="mce-responses" class="clear foot">
+                      <div class="response" id="mce-error-response" style="display:none"></div>
+                      <div class="response" id="mce-success-response" style="display:none"></div>
+                    </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                      <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_1c1543196ef746efeaee1ba01_0be410489f" tabindex="-1" value=""></div>
+                          <div class="optionalParent">
+                              <div class="clear foot">
+                                  <input type="submit" value="Get our News" name="subscribe" id="mc-embedded-subscribe" class="button">
+                                  <p class="brandingLogo" style="display:none;"><a href="http://eepurl.com/idfsm9" title="Mailchimp - email marketing made easy and fun"><img src="https://eep.io/mc-cdn-images/template_images/branding_logo_text_dark_dtp.svg"></a></p>
+                              </div>
+                          </div>
+                      </div>
+                  </form>
+                  </div>
+                  <!--End mc_embed_signup-->
+                </div>
+
+                
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <!-- INCLUDE FOOTER.PHP start -->
+      <?php include 'sections/footer.php';?>
+    <!-- INCLUDE FOOTER.PHP end -->
+
+  </body>
+</html>
