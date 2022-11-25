29a01841cd26ea1af41c8d65a808eb66e902d91d
Boris Hrnčić <borishrncic@users.noreply.github.com>
2022-11-25 21:06:40 +0100

    Added header logo navigation

--- sections/header.php
+++ sections/header.php
@@ -1,5 +1,5 @@
 <header>
-  <img src="imgs/poly-forum.svg" id="logo" alt="Poly Forum">
+  <a href="../"><img src="imgs/poly-forum.svg" id="logo" alt="Poly Forum"></a>
   <nav>
     <button id="log-in-button" onclick="window.open('https://polyforum.org/slack','_blank')">Log-in</button>  
     <button id="sign-up-button" onclick="window.open('https://polyforum.org/join','_blank')" class="featured-button">Sign-up</button>
