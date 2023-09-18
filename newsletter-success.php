a8d80c1421d698e83b47ca219a8b40a75d8a7a45
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-09-18 03:16:07 +0200

    Updated GTM Container

--- newsletter-success.php
+++ newsletter-success.php
@@ -8,9 +8,9 @@
   <body id="home">
 
     <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P8K76NG"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P8K76NG"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
     <!-- INCLUDE HEADER.PHP start -->
       <?php include 'sections/header.php';?>
